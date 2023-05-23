003bc21062e4907f1c02c423260ca8d820c44934
hendraaagil <hendraaagil@gmail.com>
2023-05-23 20:05:02 +0700

    style: change gradient color

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Credits, Features, Footer, Hero } from './components'
 
 function App() {
   return (
-    <div className="to flex min-h-screen flex-col items-center justify-center bg-green-700 bg-gradient-to-br from-green-300 text-white">
+    <div className="to flex min-h-screen flex-col items-center justify-center bg-green-700 bg-gradient-to-br from-green-400 text-white">
       <main className="flex w-full max-w-5xl flex-col items-center justify-center space-y-8 px-2 pb-8 md:px-8">
         <Hero />
         <hr className="w-full border" />
