f823c9b960a5b9a1055e7b3ae0709b0c9797e313
hendraaagil <hendraaagil@gmail.com>
2023-05-23 20:17:18 +0700

    feat: add open graph

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            siteUrl: env.VITE_SITE_URL,
+            title: 'GilBot',
+            description: 'Chat dengan GilBot sekarang! 🤘',
+          },
+        },
+      }),
+    ],
+  })
+}
