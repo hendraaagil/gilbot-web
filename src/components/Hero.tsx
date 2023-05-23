e7f1cc8c9b47d034d07abbe0180fd0440f5507f9
hendraaagil <hendraaagil@gmail.com>
2023-05-23 19:51:14 +0700

    chore: add netlify redirects

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,6 @@ import { HiChatBubbleBottomCenterText } from 'react-icons/hi2'
 import avatar from '../assets/avatar.jpg'
 
 export const Hero = () => {
-  const whatsappLink = 'https://wa.me/6285163036290?text=Halo'
-
   return (
     <section className="flex min-h-screen flex-col items-center justify-center space-y-8">
       <img
@@ -14,7 +12,7 @@ export const Hero = () => {
       <h1 className="text-4xl font-bold">GilBot</h1>
       <button
         className="flex items-center rounded bg-green-700 px-4 py-3 text-xl font-bold transition-transform hover:scale-95"
-        onClick={() => window.open(whatsappLink, '_blank', 'noopener, noreferrer')}
+        onClick={() => window.open('/chat', '_blank', 'noopener, noreferrer')}
       >
         <HiChatBubbleBottomCenterText className="mr-2" />
         Chat
