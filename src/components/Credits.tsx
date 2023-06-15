49fefc87c82d212d2aad7d086f9e3d4dfc34eedd
hendraaagil <hendraaagil@gmail.com>
2023-06-15 12:12:19 +0700

    feat: add insta command

--- src/components/Credits.tsx
+++ src/components/Credits.tsx
@@ -3,6 +3,7 @@ const credits = [
   { name: 'The Cat API', link: 'https://thecatapi.com/' },
   { name: 'Candaan API', link: 'https://candaan-api.vercel.app/' },
   { name: 'KyokoAPI', link: 'https://kyoko.rei.my.id/' },
+  { name: 'SnapSave', link: 'https://snapsave.app/download-video-instagram' },
   { name: 'jadwalsholatorg', link: 'https://github.com/lakuapik/jadwalsholatorg' },
   { name: 'summarize.tech', link: 'https://www.summarize.tech/' },
   { name: 'api-frontend.kemdikbud', link: 'https://api-frontend.kemdikbud.go.id/hit_mhs/%7Bnama_mahasiswa%7D' },
