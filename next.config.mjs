d56e9fd60eeb143175c563fe23672ae9c5be9d2b
hendraaagil <hendraaagil@gmail.com>
2024-11-14 23:37:23 +0700

    chore: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,23 @@ const withMDX = createMDX()
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      destination: 'https://github.com/hendraaagil/gilbot-web',
+      source: '/source',
+      permanent: false,
+    },
+    {
+      destination: 'https://t.me/gilchatbot',
+      source: '/chat',
+      permanent: false,
+    },
+    {
+      destination: 'https://t.me/hendraaagil',
+      source: '/support',
+      permanent: false,
+    },
+  ],
 }
 
 export default withMDX(config)
